5117a0228d53ec85a0ca216b95f2624add7463e7
Henry <liaohengyi@outlook.com>
2018-04-20 11:52:57 +0800

    deploy

--- build/dist/main/find.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-export default class Find extends React.Component {
-    render() {
-        return (React.createElement("div", null, "find"));
-    }
-}
-//# sourceMappingURL=find.js.map
--- build/dist/messages/index.js
+++
@@ -1,3 +0,0 @@
-export { ApplyDev, ApplyUnit } from './applyXHao';
-export { ApprovedDev, ApprovedUnit } from './approvedXHao';
-//# sourceMappingURL=index.js.map
