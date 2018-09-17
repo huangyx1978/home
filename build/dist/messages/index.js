e7837f62032d0a5ecd7e43f8d9335bcc36faebeb
Henry <liaohengyi@outlook.com>
2018-09-16 22:54:14 -0400

    release build

--- build/dist/messages/index.js
+++
@@ -1,4 +0,0 @@
-export { ApplyDev, ApplyUnit } from './applyXHao';
-export { ApprovedDev, ApprovedUnit } from './approvedXHao';
-export { UnitFollowInvite } from './unitFollowInvite';
-//# sourceMappingURL=index.js.map
--- build/dist/tools/index.js
+++
@@ -1,3 +0,0 @@
-export * from './unitSpan';
-export * from './userSpan';
-//# sourceMappingURL=index.js.map
